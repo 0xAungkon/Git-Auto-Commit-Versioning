e9bec00c8ed66f2b347ccfebc02f01d4066de9b6
0xAungkon <onlycoder000@gmail.com>
2024-12-15 19:57:02 +0600

    1.2 - Next

--- pygitv.py
+++ pygitv.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 
-result = subprocess.run(['git', 'log','-1', '--pretty=%B'], capture_output=True, text=True)
-last_commit_message = result.stdout.strip()
-print(last_commit_message)
-if('-' in last_commit_message):
-    
-    version=last_commit_message.split('-')[0]
-    version=version.replace('v','')
-else:
-    version='0.0'
+def return_next_version():
+    result = subprocess.run(['git', 'log','-1', '--pretty=%B'], capture_output=True, text=True)
+    last_commit_message = result.stdout.strip()
+    if('-' in last_commit_message):
+        
+        version=last_commit_message.split('-')[0]
+        version=version.replace('v','')
+    else:
+        version='0.0'
 
-version=version.strip()
+    version=version.strip()
 
-print(version)
+    try:
+        version=float(version)
+    except ValueError:
+        version=0.0
+
+    version=str(version)
+    try:
+        is_asked_for_next_v=sys.argv[1]
+    except:
+        is_asked_for_next_v=False
+    if(is_asked_for_next_v=='-rel'):
+        version_pre=int(version.split('.')[0])+1
+        version_post=int(version.split('.')[1])
+    else:
+        version_pre=int(version.split('.')[0])
+        version_post=int(version.split('.')[1])+1
+    version=str(version_pre)+'.'+str(version_post)
+    return version
+
+commit_msg=input('Enter Commit Message: ')
+version=return_next_version()
+commit_msg=version+' - '+commit_msg
+subprocess.run(['git', 'add', '.'])
+subprocess.run(['git', 'commit', '-m', commit_msg])
