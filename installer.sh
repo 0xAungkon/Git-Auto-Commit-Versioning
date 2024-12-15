0f899373d97adcca4080f71af4685abda65b5709
0xAungkon <onlycoder000@gmail.com>
2024-12-15 20:12:31 +0600

    5.3 - Test4

--- installer.sh
+++ installer.sh
@@ -1,4 +1,3 @@
 # curl https://raw.githubusercontent.com/0xAungkon/pygit-commit-versioning/main/gitautov > /tmp/gitautov;
-cp ./gitautov > /tmp/gitautov;
-chmod 777 /tmp/gitautov;
-python3 /tmp/gitautov;
+cp ./gitautov > /usr/bin/gitautov;
+chmod 777 /usr/bin/gitautov;
